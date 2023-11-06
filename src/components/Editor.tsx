58c56a065addedaaf909d7d8339eb16f7228538e
rahul <rahul.baradol.14@gmail.com>
2023-11-06 18:00:22 +0530

    minor change

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -25,7 +25,7 @@ export default function Editor() {
                 <div className='w-[80vw] flex flex-row items-center justify-between'>
                     <Code color="primary"> {verdict.message} </Code>
                     <Button onClick={() => {
-                        fetch("http://localhost:3000/api/evaluate", {
+                        fetch("https://online-judge-kzh1cj44m-rahul-baradols-projects.vercel.app/api/evaluate", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
